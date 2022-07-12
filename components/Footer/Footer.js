53c1e19159b7e136b6fb2ed13d557d78b422c34e
haoyuchou <haoyuchou123@gmail.com>
2022-07-12 17:14:43 +0800

    fix react/no-unescaped-entitie

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,22 +2,22 @@ const Footer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 bg-gray-100 gap-y-10 px-32 py-16">
       <div className="space-y-4 text-xs text-gray-900">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investers</p>
+        <h5 className="font-bold">{`ABOUT`}</h5>
+        <p>{`How Airbnb works`}</p>
+        <p>{`Newsroom`}</p>
+        <p>{`Investers`}</p>
       </div>
       <div className="space-y-4 text-xs text-gray-900">
-        <h5 className="font-bold">COMMUNITY</h5>
-        <p>Accessibility</p>
-        <p>This is an Airbnb Clone</p>
-        <p>Presents</p>
+        <h5 className="font-bold">{`COMMUNITY`}</h5>
+        <p>{`Accessibility`}</p>
+        <p>{`This is an Airbnb Clone`}</p>
+        <p>{`Presents`}</p>
       </div>
       <div className="space-y-4 text-xs text-gray-900">
-        <h5 className="font-bold">SUPPORT</h5>
-        <p>Help Center</p>
-        <p>Trust & Safety</p>
-        <p>Hi, I'm Thomas</p>
+        <h5 className="font-bold">{`SUPPORT`}</h5>
+        <p>{`Help Center`}</p>
+        <p>{`Trust & Safety`}</p>
+        <p>{`Hi, I'm Thomas`}</p>
       </div>
     </div>
   );
