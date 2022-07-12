e0f3799a84d6ebff05106989e6c8d4dc066c0156
haoyuchou <haoyuchou123@gmail.com>
2022-07-12 17:34:37 +0800

    fix medium card scale space

--- components/Anywhere/Anywhere.js
+++ components/Anywhere/Anywhere.js
@@ -5,7 +5,7 @@ const Anywhere = ({ cardsData }) => {
   return (
     <section className="pt-6">
       <h2 className="text-4xl font-semibold py-5">Live Anywhere</h2>
-      <div className="flex space-x-4 overflow-scroll scrollbar-hide pb-10">
+      <div className="flex space-x-4 overflow-scroll scrollbar-hide py-10">
         {cardsData?.map((item, idx) => {
           return <MediumCard key={idx} img={item.img} title={item.title} />;
         })}
--- components/Nearby/SmallCard.js
+++ components/Nearby/SmallCard.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const SmallCard = (props) => {
   const { img, location, distance } = props;
   return (
-    <div className="flex items-center space-x-2 mb-5 hover:scale-105 transition duration-150 ease-out cursor-pointer hover:bg-gray-200">
+    <div className="flex items-center space-x-2 mb-5 hover:scale-105 transition duration-300 ease-out cursor-pointer hover:bg-gray-200">
       <div className="relative h-16 w-16 ">
         <Image src={img} layout="fill" className="rounded-lg" alt="" />
       </div>
